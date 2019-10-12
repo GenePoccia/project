49e822675ca98ff0d4e364f6c95f4dc76c2f7673
unknown <pocciaeugenio@gmail.com>
2019-10-12 11:03:04 -0400

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import  Navbar  from './Navbar.jsx'
 
 
 class UnconnectedApp extends Component {
   render = () => {
     return (
       <div>
-          Hello - test 
+          <Navbar />
       </div>
     );
   };
